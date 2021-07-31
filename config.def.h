43339653fafe6e9f30edb18ad967a0ba4998e614
yannik <yannik.lorenz.1311@gmail.com>
2021-07-31 16:57:55 +0200

    cb | alpha patch
    Allows status bar to be transparent

--- config.def.h
+++ config.def.h
@@ -26,10 +26,19 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 //top bar second color (blue) and active window border color
 static const char col_cyan[]        = "#F69F00";
+static const unsigned int baralpha = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
