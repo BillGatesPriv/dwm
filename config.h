1996d5d85ab4fda9109c57aa69ea35d7fea96ecd
yannik <yannik.lorenz.1311@gmail.com>
2021-07-31 15:33:38 +0200

    cb | rofi conf

--- config.h
+++ config.h
@@ -5,22 +5,27 @@
 #define TERMCLASS "St"
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappoh    = 10;        /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=12", "fontawesome:size=12" };
+static const char dmenufont[]       = "monospace:size=11";
+//background color
 static const char col_gray1[]       = "#222222";
+//incactive window border color
 static const char col_gray2[]       = "#444444";
+//font color
 static const char col_gray3[]       = "#bbbbbb";
+//current tag and current window font color
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+//top bar second color (blue) and active window border color
+static const char col_cyan[]        = "#F69F00";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -81,7 +86,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *roficmd[]  = { "rofi", "-show", "drun", "-drun-match-fields", "exec", "display-drun", "'RUN:'" NULL};
+static const char *roficmd[]  = { "rofi", "-show", "drun", "-drun-match-fields", "exec", "display-drun", "'RUN:'", NULL};
 static const char *rofipopcmd[]  = { "rofi", "-show", "run", "-config", "~/.config/rofi/config-center.rasi", NULL};
 static const char *termcmd[]  = { "st", NULL };
 
